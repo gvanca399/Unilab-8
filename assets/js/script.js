d660276fa14c24e37b4ae340514d8ba238e928fd
Gvantsa Baidoshvili <64689915+gvanca399@users.noreply.github.com>
2022-08-18 19:18:39 +0400

    updates

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let User = {
+    myName: "Gvantsa",
+    myLastName: "Baidoshvili",
+    myAge: 20,
+    myEmail: "gvantsa.baidoshvili.1@iliauni.edu.ge",
+    city: "sighnaghi"
+}
+
+console.log(User.myName, User.myLastName, User.myAge, User.myEmail, User.city)
